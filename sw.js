e277d257dbdd22a39fe38814a635bbe7bde86735
李晓娟 <lixiaojuan001@lianjia.com>
2018-12-19 11:17:08 +0800

    fix bugs

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'sylvia_cache_v1.0.0'
 const urlsToCache = [
   './worker.js',
-  './static/img/cat.png'
+  // './static/img/cat.png'
 ]
 const fetchUrl = [
   './static/img/cat.png'
@@ -20,17 +20,32 @@ self.addEventListener('fetch', function(e){
   console.log(e)
   let requestUrl = e.request.url
   if(fetchUrl.indexOf(requestUrl) > -1){
-    caches.open(CACHE_NAME).then(cache => {
-      return fetch('./static/img/dog.png').then(res => {
-        return cache.put('./static/img/cat.png', res)
-      })
-    })
+    // caches.open(CACHE_NAME).then(cache => {
+    //   return fetch('./static/img/cat.png').then(res => {
+    //     return cache.put('./static/img/dog.png', res)
+    //   })
+    // })
     
     e.responseWith(
-      // caches.open(CACHE_NAME).then(cache => {
-      //   cache.put(e.request, )
-      // })
-      fetch('./static/img/dog.png')
+      caches.match(e.request).then(res => {
+        console.log(res)
+        if(res){
+          return res
+        }
+        let reqClone = e.request.clone()
+        return fetch('./static/img/dog.png').then(response => {
+          console.log(response)
+          if(!response || response.status !== 200){
+            return response
+          }
+
+          let responseClone = response.clone()
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(e.request, responseClone)
+          })
+          return response
+        })
+      })
     )
   }
 })
